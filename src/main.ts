cbc68ecfe4334fe37e8ff4e1408c8ba8d1becb2e
Maxim Ryabykh <ryabykhms@gmail.com>
2023-02-04 23:07:05 +0300

    feat: added using port from env and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+config();
+
+const port = process.env.PORT || '4000';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(port);
 }
 bootstrap();
