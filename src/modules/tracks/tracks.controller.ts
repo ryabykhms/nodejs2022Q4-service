85ec84a6c9dcc1cd2b491a2def765bbc52d7d172
Maxim Ryabykh <ryabykhms@gmail.com>
2023-02-05 16:28:00 +0300

    feat: implemented tracks controller

---
+++ src/modules/tracks/tracks.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
+import { NotFoundInterceptor } from 'src/interceptors/not-found.interceptor';
+import { TracksService } from './tracks.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+import { Track } from './entities/tracks.entity';
+
+@Controller('track')
+@UseInterceptors(NotFoundInterceptor)
+export class TracksController {
+  constructor(private readonly tracksService: TracksService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createTrackDto: CreateTrackDto): Track {
+    return this.tracksService.create(createTrackDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.tracksService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id', ParseUUIDPipe) id: string): Track {
+    return this.tracksService.getById(id);
+  }
+
+  @Put(':id')
+  updatePassword(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() data: UpdateTrackDto,
+  ): Track {
+    return this.tracksService.update(id, data);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseUUIDPipe) id: string): void {
+    return this.tracksService.delete(id) ? null : undefined;
+  }
+}
