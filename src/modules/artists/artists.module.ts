494ecadef578375945d61fc9c8ab60b827eb7664
Maxim Ryabykh <ryabykhms@gmail.com>
2023-02-05 16:30:10 +0300

    feat: integrated tracks service to artists module

--- src/modules/artists/artists.module.ts
+++ src/modules/artists/artists.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AlbumsStorage } from '../albums/storages/albums.storage';
+import { AlbumsModule } from '../albums/albums.module';
+import { TracksModule } from '../tracks/tracks.module';
 import { ArtistsController } from './artists.controller';
 import { ArtistsService } from './artists.service';
 import { ArtistsStorage } from './storages/artists.storage';
 
 @Module({
+  imports: [TracksModule, AlbumsModule],
   controllers: [ArtistsController],
-  providers: [ArtistsService, ArtistsStorage, AlbumsStorage],
+  providers: [ArtistsService, ArtistsStorage],
 })
 export class ArtistsModule {}
--- src/modules/artists/artists.service.ts
+++ src/modules/artists/artists.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { AlbumsStorage } from '../albums/storages/albums.storage';
+import { Inject, Injectable } from '@nestjs/common';
+import { AlbumsService } from '../albums/albums.service';
+import { TracksService } from '../tracks/tracks.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 import { Artist } from './entities/artists.entity';
@@ -8,29 +9,34 @@ import { ArtistsStorage } from './storages/artists.storage';
 @Injectable()
 export class ArtistsService {
   constructor(
-    private readonly artists: ArtistsStorage,
-    private readonly albums: AlbumsStorage,
+    private readonly collection: ArtistsStorage,
+    @Inject(AlbumsService) private albumsService: AlbumsService,
+    @Inject(TracksService) private tracksService: TracksService,
   ) {}
 
   create(createArtistDto: CreateArtistDto): Artist {
-    return this.artists.create(createArtistDto);
+    return this.collection.create(createArtistDto);
   }
 
   getAll(): Artist[] {
-    return this.artists.getAll();
+    return this.collection.getAll();
   }
 
   getById(id: string): Artist {
-    return this.artists.getById(id);
+    return this.collection.getById(id);
   }
 
   update(id: string, data: UpdateArtistDto): Artist {
-    return this.artists.update(id, data);
+    return this.collection.update(id, data);
   }
 
   delete(id: string): boolean {
-    const isDeleted = this.artists.delete(id);
-    isDeleted && this.albums.deleteByArtistId(id);
+    const isDeleted = this.collection.delete(id);
+
+    if (isDeleted) {
+      this.albumsService.removeArtistId(id);
+      this.tracksService.removeArtistId(id);
+    }
 
     return isDeleted;
   }
