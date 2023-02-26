33241140f7c53d65d3c65089db7cedd3c4ce3ec0
Maxim Ryabykh <ryabykhms@gmail.com>
2023-02-27 01:16:09 +0300

    feat: implemented exception filter

---
+++ src/exception-filters/http-exception-filter.ts
@@ -0,0 +1,24 @@
+import { ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+import { Catch } from '@nestjs/common/decorators';
+import { HttpException } from '@nestjs/common/exceptions';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+    const statusCode = exception.getStatus();
+    const message = exception.message;
+    const path = request.url;
+    const timestamp = new Date().toISOString();
+
+    response.status(statusCode).json({
+      statusCode,
+      message,
+      path,
+      timestamp,
+    });
+  }
+}
